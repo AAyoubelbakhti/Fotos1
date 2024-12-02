37dc015bff7e19707a4bcb96506da6e8e02a6fc2
AAyoubelbakhti <ayoubelbak13@gmail.com>
2024-12-02 16:17:51 +0100

    pt1

--- app/src/main/java/com/ayoub/FOTOS1/MainActivity.java
+++ app/src/main/java/com/ayoub/FOTOS1/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ayoub.FOTOS1;
 
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button button = findViewById(R.id.button);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
